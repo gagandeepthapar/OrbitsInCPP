22d3e9bb36d894424cc206cf9f0383e6f34b16b3
gaganthapar12309 <gsthapar@gmail.com>
2023-02-21 11:42:33 -0800

    made global vars constant (as they will never change)

--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
+const int EARTH_MU = 398600;
+const int EARTH_RAD = 6378;
+const int SEC_TO_MIN = 60;
+const int SEC_TO_HR = 3600;
+const int SEC_TO_DAY = 86400;
+
+using namespace std;
 int main(){
 
-	std::cout << "Orbital Mechanics!" << std::endl;
-	return 0;
+  cout << "Orbits!" << endl;
+  cout << "Testing testing!" << endl;
+  cout << "Mu of Earth: " << EARTH_MU << endl;
+
+
+  vector<double> state0 = {6878, 0, 0, 0, sqrt(EARTH_MU/6878), 0};
+
+  for (float i : state0){
+    cout << i << " ";
+  }
+
+  cout << endl;
+
+  return 0;
+
 
 }
